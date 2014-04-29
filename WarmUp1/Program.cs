3213eafb5f81127007528f95826130fe6f2bd3f8
Youngs285 <nitronite500@gmail.com>
2014-04-29 09:31:11 -0400

    WarmUp1

---
+++ WarmUp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarmUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum = 0, n = 0, SumSquared = 0, Difference = 0;
+
+            for (int i = 0; i <= 100; i ++)
+            {
+                n += i * i;
+                Sum += i;
+            }
+
+            SumSquared = Sum * Sum;
+
+            Difference = SumSquared - n;
+
+            Console.WriteLine("{0}-{1}={2}",SumSquared,n,Difference);
+        }
+    }
+}
